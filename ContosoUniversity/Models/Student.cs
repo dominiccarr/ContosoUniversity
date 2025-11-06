a7a969dc8c4727241c4bf6564b5e01ed94a297cf
Dominic Carr <6775298+dominiccarr@users.noreply.github.com>
2025-11-06 16:08:10 +0000

    Changes to support

--- ContosoUniversity/Models/Student.cs
+++ ContosoUniversity/Models/Student.cs
@@ -6,31 +6,12 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ContosoUniversity.Models
 {
-    public class Student
+    public class Student : Person
     {
-        public int ID { get; set; }
-
-        [StringLength(50, MinimumLength =3)]
-        [DisplayName("Last Name")]
-        public string LastName { get; set; }
-        [StringLength(50, MinimumLength = 3)]
-        [Column("FirstName")]
-        [DisplayName("First Name")]
-        public string FirstMidName { get; set; }
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}", ApplyFormatInEditMode = true)]
         [DisplayName("Enrollment Date")]
         public DateTime EnrollmentDate { get; set; }
-
-        // Calculated Property
-        [Display(Name = "Full Name")]
-        public string FullName
-        {
-            get
-            {
-                return LastName + ", " + FirstMidName;
-            }
-        }
 
 
         public virtual ICollection<Enrollment>? Enrollments { get; set; }
